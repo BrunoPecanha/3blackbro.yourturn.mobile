59d62c5d34902f36e31debde24708a836c46b878
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-08-21 22:32:12 -0300

    🚧 add the splash screen to the routing system

--- lib/main/config/routing.config.dart
+++ lib/main/config/routing.config.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:the_3_black_bro/presentation/screens/test.screen.dart';
+import 'package:the_3_black_bro/presentation/screens/splash.screen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  '/': (context) => const Test(),
+  '/': (context) => Splash(),
 };
