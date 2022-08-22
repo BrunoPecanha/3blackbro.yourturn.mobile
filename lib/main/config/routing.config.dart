6a7899ffc5db2014b23643c0be59fe7ab5513206
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-08-21 22:02:19 -0300

    ✨ create a routing config file to hold all routes

---
+++ lib/main/config/routing.config.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:the_3_black_bro/presentation/screens/test.screen.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  '/': (context) => const Test(),
+};
