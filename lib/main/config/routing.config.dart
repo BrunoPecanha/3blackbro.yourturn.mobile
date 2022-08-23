67bb24bc51966a2b86ad555200ffe42da9a17f50
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-08-23 12:47:15 -0300

    🚧 add the login screen in the routing system

--- lib/main/config/routing.config.dart
+++ lib/main/config/routing.config.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:the_3_black_bro/presentation/screens/login.screen.dart';
 import 'package:the_3_black_bro/presentation/screens/splash.screen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   '/': (context) => Splash(),
+  '/login': (context) => Login(),
 };
